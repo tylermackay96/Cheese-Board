52d747343e1efe6f2f0eaac6156ddcdcadbadc8b
Tyler <tyler.mackay@barclays.com>
2023-02-17 14:36:53 +0000

    Cheese and Board Models Added

--- models/Board.js
+++ models/Board.js
@@ -1,2 +1,10 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db/connection.js");
+
+const Board = sequelize.define(Board,{
+    type: DataTypes.STRING,
+    description: DataTypes.STRING,
+    rating: DataTypes.INTERGER
+});
+
+moduel,exports = Board;
--- models/Cheese.js
+++ models/Cheese.js
@@ -1,2 +1,9 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db/connection.js");
+
+const Cheese = sequelize.define(Cheese,{
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+});
+
+moduel,exports = Cheese;
